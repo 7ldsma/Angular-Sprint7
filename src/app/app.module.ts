d3f7bcdfde3fa61829d8098bc34b1eca706198ef
Alvaro <alvaro.ledesmav@gmail.com>
2023-09-13 13:12:59 +0200

    panel component html added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { PanelComponent } from './home/components/home/panel/panel.component';
 
 @NgModule({
   declarations: [
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
+import { PanelComponent } from './components/home/panel/panel.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    PanelComponent
   ],
   exports: [
-    HomeComponent
+    HomeComponent,
+    PanelComponent
   ],
   imports: [
     CommonModule,
