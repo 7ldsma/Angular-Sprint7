0f2c93aa96085fe3bdf496fcc2c58f29e2863f2a
Alvaro <alvaro.ledesmav@gmail.com>
2023-09-13 16:29:02 +0200

    panel componen input changes

--- src/app/home/components/home/panel/panel.component.ts
+++ src/app/home/components/home/panel/panel.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class PanelComponent {
 
+  public pages: number = 1;
+  public languages: number = 1;
+
+  increasePages (): void {
+    this.pages += 1;
+  }
+
+  decreasePages (): void {
+    if (this.pages > 1) {
+      this.pages -= 1;
+    }
+  }
+
+  increaseLanguages (): void {
+    this.languages += 1;
+  }
+
+  decreaseLanguages (): void {
+    if (this.languages > 1) {
+      this.languages -= 1;
+    }
+  }
+
 }
